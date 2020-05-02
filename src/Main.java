085127f33c526f41331b0522119bd858cfe7146a
legendmoments23@gmail.com <erlanmatenov>
2020-05-02 17:31:35 +0600

    first expl

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Комментарий к нашему коду
+        /* dfsdfdsf
+        fsdfdsf
+        dsfsd
+         */
+        int myNumber = 500;
+
+        System.out.println(myNumber);
+        System.out.println("Hello world!");
+        System.out.println(myNumber);
+        System.out.println(myNumber);
+        System.out.println(myNumber);
+        System.out.println(myNumber);
+
+        myNumber = 3;
+        System.out.println(myNumber);
+
+        boolean isRainy = false;
+
+        double myDigit = 78.2;
+        System.out.println(myDigit);
+
+        double result = myNumber + myDigit;
+        System.out.println(result);
+
+        String myName = "Alexey";
+        System.out.println(myName);
+
+        System.out.println(myNumber + myNumber * myDigit);
+        System.out.println(myName + myNumber + myDigit); // + склеивание (concatenation)
+
+        final String test = myName + 8;
+        System.out.println(test);
+        final double pi = 3.14;
+
+        String nickName = "Loki_KG is asked a question";
+        String text = " now";
+        System.out.println(nickName + text);
+    }
+}
